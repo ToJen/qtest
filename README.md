5693d2418ad06f3083fb503b81a8d5e2d8862343
Tojen <tomisinjenrola@gmail.com>
2020-02-24 02:21:22 +0000

    calculate probability amplitude for qft qasm

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# qc_mentorship_task
+# qtest
+Computes the probability that the ouput of a quantum citcuit is correct.
 
+## Requirements
+- `python >= 3`
+
+## Install Dependencies
+- `pip install -r requirements.txt`
+
+## Run
+- `python main.py`
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from qiskit import QuantumCircuit
 from qiskit import QiskitError, execute, BasicAer
 from qiskit.tools.visualization import plot_histogram
-# from qiskit.providers.aer.noise import NoiseModel
 
-sim_backend = BasicAer.get_backend('qasm_simulator')
-# noise_model = NoiseModel.from_backend(sim_backend)
+
+sim_backend = BasicAer.get_backend('statevector_simulator')
 
 circ = QuantumCircuit.from_qasm_file("qasm/qft.qasm")
-circ.draw(output='mpl')
+print(circ)
 
-job_sim = execute(circ, sim_backend)
-sim_result = job_sim.result()
+sim_result = execute(circ, sim_backend).result()
 
-counts = sim_result.get_counts(circ)
-print(counts)
-print(plot_histogram(counts))
+probAmpltitudes = sim_result.get_statevector(circ)
+
+print(probAmpltitudes)
 
